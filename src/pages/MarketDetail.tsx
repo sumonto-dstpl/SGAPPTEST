e23c2041a3a5e7a08e547f84611722a96b716138
sumonto pyne <156449764+sumonto-dstpl@users.noreply.github.com>
2026-08-14 12:15:22 +0530

    Updated MarketDetail.tsx

--- src/pages/MarketDetail.tsx
+++ src/pages/MarketDetail.tsx
@@ -350,7 +350,7 @@ function EditShopModal({ shop, onClose, onRequestCollect }: { shop: Shop; onClos
 
   totalPeriods = Math.max(1, totalPeriods);
 
-  const currentDue = Number(form.monthlyRent) * totalPeriods;
+  const currentDue = (Number(form.monthlyRent) * totalPeriods)-shop.paidrent;
 
   setForm(p => ({
     ...p,
