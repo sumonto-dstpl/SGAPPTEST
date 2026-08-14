4fc0d06b704b8f6f468bd8662ed4beab343f0650
sumonto pyne <156449764+sumonto-dstpl@users.noreply.github.com>
2026-08-14 12:45:33 +0530

    Updated MarketDetail.tsx

--- src/pages/MarketDetail.tsx
+++ src/pages/MarketDetail.tsx
@@ -361,7 +361,7 @@ function EditShopModal({ shop, onClose, onRequestCollect }: { shop: Shop; onClos
   shop.shopType,
   form.endDate,
   form.monthlyRent,
-    form.paidRent
+    form.paidRent,
 ]);
 
 
